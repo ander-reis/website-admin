43f35301f432345740b6c1ccdc378fc9d8aa9fca
Anderson <anderson@sinprosp.org.br>
2019-09-09 10:14:16 -0300

    Atualizado admin.scss

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements JWTSubject
     protected $table = 'tb_sinpro_usuarios';
 
     public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
