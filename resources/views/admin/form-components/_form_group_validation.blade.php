18f927f10efedc0952c49194f31b0a4f58488d90
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-01 16:54:34 -0300

    ajustado coluna para mobile

--- resources/views/admin/form-components/_form_group_validation.blade.php
+++ resources/views/admin/form-components/_form_group_validation.blade.php
@@ -7,14 +7,14 @@
         </div>
         <div class="card-body row">
             @if(isset($block_one))
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <div class="form-group">
                     {{$block_one}}
                 </div>
             </div>
             @endif
             @if(isset($block_two))
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <div class="form-group">
                     {{$block_two}}
                 </div>
