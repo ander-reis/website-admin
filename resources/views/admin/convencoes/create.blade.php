ac308d61db2baad07630dd33bdeca584bba581e7
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-04 09:55:57 -0300

    botao voltar no create da Clausulas e Convencoes

--- resources/views/admin/convencoes/create.blade.php
+++ resources/views/admin/convencoes/create.blade.php
@@ -10,9 +10,9 @@
 
         @include('admin.convencoes._form')
 
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
 
-        {{-- <a href="{{ route('admin.convencao.index') }}" class="button btn btn-danger">Voltar</a> --}}
+        <a href="{{ route('admin.convencao.index', ['id' => $model->fl_entidade]) }}" class="button btn btn-danger">Voltar</a>
 
         {{ Form::close() }}
 
