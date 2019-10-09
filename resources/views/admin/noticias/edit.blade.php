59ede13b47ee5f4bfa8f41b4a9f23dcdcef3f1a0
Ronaldo Araujo <=ronaldo@sinprosp.org.br>
2019-10-09 15:09:51 -0300

    voltar para pagina de edicao

--- resources/views/admin/noticias/edit.blade.php
+++ resources/views/admin/noticias/edit.blade.php
@@ -11,7 +11,7 @@
         @include('admin.noticias._form')
 
         <button type="submit" class="btn btn-primary">Salvar</button>
-        <a href="{{ route('admin.noticias.index') }}" class="button btn btn-danger">Voltar</a>
+        <a href="javascript:history.go(-1)" class="button btn btn-danger">Voltar</a>
 
         {{ Form::close() }}
     </div>
--- resources/views/admin/paginas-principais/edit.blade.php
+++ resources/views/admin/paginas-principais/edit.blade.php
@@ -5,7 +5,7 @@
         {{ Form::model($model, ['route' => ['admin.paginas-principais.update', $model->id_pagina], 'method' => 'PUT']) }}
         @include('admin.paginas-principais._form')
         <button type="submit" class="btn btn-primary">Salvar</button>
-        <a href="{{ route('admin.paginas-principais.index') }}" class="button btn btn-danger">Voltar</a>
+        <a href="javascript:history.go(-1)" class="button btn btn-danger">Voltar</a>
         {{ Form::close() }}
     </div>
 @endsection()
