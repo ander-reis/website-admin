4c38873510833ae31d4efb4b60167eff312531c7
Anderson <ander-reis@hotmail.com>
2019-10-22 10:09:36 -0300

    Implementação complementar policies em AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,26 @@
 namespace App\Providers;
 
 use App\Auth\CustomUserProvider;
+use App\Models\BoletimCadastro;
 use App\Models\Convencoes;
+use App\Models\ConvencoesClausulas;
+use App\Models\HomePage;
+use App\Models\Intro;
 use App\Models\Noticias;
+use App\Models\NoticiasCategoria;
+use App\Models\OrdemNoticias;
+use App\Models\PaginasPrincipais;
+use App\Models\Slider;
+use App\Policies\BoletimCadastroPolicy;
+use App\Policies\ClausulasPolicy;
 use App\Policies\ConvencoesPolicy;
+use App\Policies\HomePagePolicy;
+use App\Policies\IntroPolicy;
+use App\Policies\NoticiasCategoriaPolicy;
 use App\Policies\NoticiasPolicy;
+use App\Policies\OrdemNoticiasPolicy;
+use App\Policies\PaginasPrincipaisPolicy;
+use App\Policies\SliderPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -19,7 +35,15 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Noticias::class => NoticiasPolicy::class,
+        NoticiasCategoria::class => NoticiasCategoriaPolicy::class,
+        OrdemNoticias::class => OrdemNoticiasPolicy::class,
         Convencoes::class => ConvencoesPolicy::class,
+        ConvencoesClausulas::class => ClausulasPolicy::class,
+        Slider::class => SliderPolicy::class,
+        Intro::class => IntroPolicy::class,
+        HomePage::class => HomePagePolicy::class,
+        PaginasPrincipais::class => PaginasPrincipaisPolicy::class,
+        BoletimCadastro::class => BoletimCadastroPolicy::class
     ];
 
     /**
